d25cb35b2a05a83eb2b4d2d8cbdb82cd45e9e05f
Danail Krasimirov <dsurudzhiyski@hotmail.com>
2020-04-02 23:46:30 +0200

    new progress

--- movie/models.py
+++ movie/models.py
@@ -29,9 +29,26 @@ class Movie(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=1)
     language =  models.CharField(choices=LANGUAGE_CHOICES, max_length=2)
     status  = models.CharField(choices=STATUS_CHOICES,max_length=2)
+    cast = models.CharField(max_length=100)
     year_of_production = models.DateField()
     views_count = models.IntegerField(default=0)
 
 
     def __str__(self):
         return self.title
+
+LINK_CHOICES = (
+    ('D','DOWNLOAD LINK'),
+    ('W','WATCH LINK')
+)
+
+class MovieLinks(models.Model):
+    movie = models.ForeignKey(Movie,related_name='movie_watch_link', on_delete=models.CASCADE)
+    type  = models.CharField(choices=LINK_CHOICES,max_length=1)
+    links = models.URLField()
+
+
+    def __str__(self):
+        return str(self.movie)
+
+
--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, MovieLinks
 
 
 # Create your views here.
@@ -16,3 +16,19 @@ class MovieList(ListView):
 
 class MovieDetails(DetailView):
     model = Movie
+    
+
+    def get_object(self):
+        object = super(MovieDetails, self).get_object()
+        object.views_count += 1
+        object.save()
+        return object
+
+
+    def get_context_data(self, **kwargs):
+        context = super(MovieDetails, self).get_context_data(**kwargs)
+        context['links'] = MovieLinks.objects.filter(movie = self.get_object())
+        return context
+
+
+
